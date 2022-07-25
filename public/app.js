6b92b62f2d22cbc3b33c99e5abe3dd1772297fb2
chiragthakuri <83844592+chiragthakuri@users.noreply.github.com>
2022-07-25 10:57:05 -0500

    using tuples to remove redudancy in the form

--- public/app.js
+++ public/app.js
@@ -11,12 +11,42 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne.name);
+//enumns
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docThree = {
+    uid: 1,
+    resourceType: ResourceType.AUTHOR,
+    data: { name: 'chirag' },
+};
+console.log(docThree);
+const docFour = {
+    uid: 2,
+    resourceType: ResourceType.PERSON,
+    data: { title: 'widl wild west' },
+};
+console.log(docThree, docFour);
+//tuples
--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,56 @@ const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
   let doc: HasFormatter;
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, 'end');
 });
+
+const addUID = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: 'yoshi', age: 40 });
+
+console.log(docOne.name);
+
+//enumns
+enum ResourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR,
+  PERSON,
+}
+
+interface Resource<T> {
+  uid: number;
+  resourceType: ResourceType;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceType: ResourceType.AUTHOR,
+  data: { name: 'chirag' },
+};
+
+console.log(docThree);
+
+const docFour: Resource<object> = {
+  uid: 2,
+  resourceType: ResourceType.PERSON,
+  data: { title: 'widl wild west' },
+};
+
+console.log(docThree, docFour);
+
+//tuples
