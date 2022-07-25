e461839406306fb9366873aa69e0f4b7a2a06630
chiragthakuri <83844592+chiragthakuri@users.noreply.github.com>
2022-07-25 09:38:06 -0500

    redendering html

--- public/app.js
+++ public/app.js
@@ -1,17 +1,22 @@
 import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice('Mario', 'work on the game', 450);
-const invTwo = new Invoice('Dave', 'work on the cartoon', 550);
-console.log(invOne, invTwo);
-let invoices = [invOne, invTwo];
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/payment.js';
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
 import { Invoice } from './classes/invoice.js';
-
-const invOne = new Invoice('Mario', 'work on the game', 450);
-const invTwo = new Invoice('Dave', 'work on the cartoon', 550);
-
-console.log(invOne, invTwo);
-
-let invoices: Invoice[] = [invOne, invTwo];
-
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -18,7 +10,19 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+//list template instance
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, 'end');
 });
